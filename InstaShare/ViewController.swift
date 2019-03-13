e442ffbd0cb96beb7965ff1b47d8255f91d0bc5c
William Bui <williambui@williams-macbook-pro.local>
2019-03-13 11:24:09 -0400

    Initial Commit

---
+++ InstaShare/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InstaShare
+//
+//  Created by William Bui on 3/13/19.
+//  Copyright © 2019 William Bui. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
