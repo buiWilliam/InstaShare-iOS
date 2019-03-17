5f9f70fca7401972f0ac493cc4838459b85fbfa5
William Bui <tug69403@temple.edu>
2019-03-17 12:00:25 -0400

    added a login screen and sign up screen

---
+++ InstaShare/SignUpViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SignUpViewController.swift
+//  InstaShare
+//
+//  Created by William Bui on 3/16/19.
+//  Copyright © 2019 William Bui. All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
